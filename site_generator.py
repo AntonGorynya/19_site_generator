0a36d06f9542eaf583e6d4eed2b6822b6908dbe3
Gorynya Anton <g84086619@china.huawei.com>
2017-11-18 20:14:48 +0300

    delete extra files, add requirements

--- site_generator.py
+++ site_generator.py
@@ -1,5 +1,6 @@
 import json
 from jinja2 import Environment, FileSystemLoader
+from livereload import Server
 import markdown
 import glob
 import os
@@ -11,8 +12,9 @@ def get_md_list():
 
 def convert_md2html(path):
     with open(path, 'r', encoding='utf-8') as md_data:
+        extension_param = 'markdown.extensions.codehilite'
         html_data = markdown.markdown(md_data.read(),
-                                      extensions=['markdown.extensions.codehilite'])
+                                      extensions=[extension_param])
     file_name, file_extension = os.path.splitext(path)
     with open('{}.html'.format(file_name), 'w', encoding="utf8") as out_file:
         out_file.write(html_data)
@@ -31,12 +33,11 @@ def load_templates():
     return index_template, page_template
 
 
-if __name__ == '__main__':
+def make_site():
     index_template, page_template = load_templates()
     context_data = load_data()
     with open('index.html', 'w', encoding="utf8") as out_file:
         out_file.write(index_template.render(context_data))
-
     for md_file in get_md_list():
         convert_md2html(md_file)
     list_html_files = glob.glob('./articles/*/*.html')
@@ -45,3 +46,10 @@ if __name__ == '__main__':
             context_data = {'context': in_file.read()}
         with open(html_file, 'w', encoding="utf8") as out_file:
             out_file.write(page_template.render(context_data))
+
+
+if __name__ == '__main__':
+    make_site()
+    server = Server()
+    server.watch('templates/*.html', make_site)
+    server.serve(root='site/')
